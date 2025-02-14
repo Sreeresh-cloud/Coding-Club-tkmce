d1ffbae78df70669bb199487837d4756a0094f3a
Sreeresh SS <sreereshss2@gmail.com>
2025-02-14 11:42:57 +0530

    Update Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,14 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Sidebar } from "primereact/sidebar";
-import { Link } from "react-router-dom";
 import Header from "./Header";
 import { Dialog } from "primereact/dialog";
 
 const Events = () => {
-    const [visible, setVisible] = useState(false);
     const [events, setEvents] = useState([]);
+    const [allEvents, setAllEvents] = useState([]);
     const [dialogVisible, setDialogVisible] = useState(false);
+    const [successDialogVisible, setSuccessDialogVisible] = useState(false);
+    const [errorDialogVisible, setErrorDialogVisible] = useState(false);
+
     const [formData, setFormData] = useState({
         name: '',
         branch: '',
@@ -31,7 +32,20 @@ const Events = () => {
             }
         };
 
+        const fetchPastEvents = async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/events`);
+                const currentDate = new Date();
+                const pastEvents = response.data.filter(event => new Date(event.eventDate) < currentDate);
+                setAllEvents(pastEvents);
+            } catch (error) {
+                console.error("Error fetching past events", error);
+            }
+        };
+        
+
         fetchEvents();
+        fetchPastEvents();
     }, []);
 
     const handleRegisterClick = (event) => {
@@ -39,7 +53,6 @@ const Events = () => {
             ...formData,
             eventTitle: event.eventName,    
             eventDate: new Date(event.eventDate).toISOString(), 
-
         });
         setDialogVisible(true); 
     };
@@ -64,9 +77,10 @@ const Events = () => {
                 eventTitle: '',
                 eventDate: ''   
             }); 
+            setSuccessDialogVisible(true);
         } catch (error) {
             console.error("Error registering for event:", error);
-            
+            setErrorDialogVisible(true);
         }
     };
 
@@ -74,22 +88,11 @@ const Events = () => {
         <>
             <div>
                 <Header />
-                <Sidebar
-                    visible={visible}
-                    position="right"
-                    onHide={() => setVisible(false)}
-                >
-                    <div className="flex flex-col gap-7 items-center">
-                        <Link to="/" onClick={() => setVisible(false)}>Home</Link>
-                        <Link to="/events" onClick={() => setVisible(false)}>Events</Link>
-                        <Link to="/execom" onClick={() => setVisible(false)}>Execom</Link>
-                        <Link to="/contacts" onClick={() => setVisible(false)}>Contact</Link>
-                    </div>
-                </Sidebar>
             </div>
             <div className="flex-auto p-7 mb-7 sm:mb-20 sm:px-14">
+                {/* Upcoming Events Section */}
                 <div className="flex sm:mt-7 mb-24">
-                    <div className="font-black text-4xl sm:text-6xl">EVENTS</div>
+                    <div className="font-black text-4xl sm:text-6xl">UPCOMING EVENTS</div>
                     <div className="flex-auto border-b-4 mb-2 ml-2"></div>
                 </div>
 
@@ -127,6 +130,49 @@ const Events = () => {
                         ))
                     ) : (
                         <p>No upcoming events currently</p>
+                    )}
+                </div>
+
+                {/* All Events Section */}
+                <div className="flex sm:mt-7 mb-24 mt-20">
+                    <div className="font-black text-4xl sm:text-6xl">PAST EVENTS</div>
+                    <div className="flex-auto border-b-4 mb-2 ml-2"></div>
+                </div>
+
+                <div className="flex flex-wrap justify-center gap-4">
+                    {allEvents.length > 0 ? (
+                        allEvents.map(event => (
+                            <div key={event._id} className="event-card mb-6 p-5 w-80 border rounded-lg shadow-lg transition-transform duration-200 hover:scale-105 bg-gradient-to-b from-gray-700 to-black text-white text-center">
+                                {event.eventImage && (
+                                    <img 
+                                        src={`data:image/png;base64,${event.eventImage}`} 
+                                        alt={event.eventName} 
+                                        className="w-100 h-120 object-contain rounded-t-lg mb-4"
+                                    />
+                                )}
+                                <h3 className="text-xl font-semibold mb-2 uppercase underline">{event.eventName}</h3>
+                                <p className="mb-2 font-normal text-gray-400">{event.eventDescription}</p>
+                                <p className="text-gray-200">
+                                    <strong>Date: </strong> 
+                                    <span className="text-red-500">
+                                        {new Date(event.eventDate).toLocaleString("en-US", {
+                                            year: "numeric",
+                                            month: "long",
+                                            day: "numeric",
+                                            hour: "2-digit",
+                                            minute: "2-digit",
+                                            hour12: true
+                                        })}
+                                    </span>
+                                </p>
+                                <p className="text-gray-200"><strong>Venue:</strong> {event.eventVenue}</p>
+                                <p className="text-gray-200"><strong>Mode:</strong> {event.eventMode}</p>
+
+                                <button onClick={() => handleRegisterClick(event)} className="mt-4 bg-white text-green-500 font-semibold py-2 px-4 rounded hover:bg-gray-200">Register</button>
+                            </div>
+                        ))
+                    ) : (
+                        <p>No events available</p>
                     )}
                 </div>
             </div>
@@ -178,8 +224,39 @@ const Events = () => {
                     </form>
                 </div>
             </Dialog>
+            
+            <Dialog
+                header="Registration Successful"
+                visible={successDialogVisible}
+                style={{ width: '30vw' }}
+                onHide={() => setSuccessDialogVisible(false)}
+            >
+                <p>Your registration was successful!</p>
+                <button
+                    onClick={() => setSuccessDialogVisible(false)}
+                    className="mt-4 w-32 bg-gradient-to-r from-green-500 to-blue-500 text-white font-semibold py-2 px-4 rounded hover:scale-105 transition-transform duration-200 transform translate-x-2"
+                >
+                    Close
+                </button>
+            </Dialog>
+
+            <Dialog
+                header="Registration Failed"
+                visible={errorDialogVisible}
+                style={{ width: '30vw' }}
+                onHide={() => setErrorDialogVisible(false)}
+            >
+                <p>Sorry, there was an error registering for the event. Please try again later.</p>
+                <button
+                    onClick={() => setErrorDialogVisible(false)}
+                    className="mt-4 w-32 bg-gradient-to-r from-red-500 to-orange-500 text-white font-semibold py-2 px-4 rounded hover:scale-105 transition-transform duration-200 transform translate-x-2"
+                >
+                    Close
+                </button>
+            </Dialog>
         </>
     );
 };
 
 export default Events;
+
